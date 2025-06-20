021affa9b1114ca82655b866bde0e9be1769e292
Bommen258 <binhnbhe172640@fpt.edu.vn>
2025-06-20 23:44:34 +0700

    update blog

--- Models/DTO/BlogDTO.cs
+++ Models/DTO/BlogDTO.cs
@@ -5,23 +5,30 @@
 		public int BlogImageID { get; set; }
 		public int? ImageID { get; set; }
 		public string ImageURL { get; set; }
+		public string ImageAlt { get; set; }
 	}
 	public class BlogDto
 	{
 		public int BlogID { get; set; }
 		public string BlogName { get; set; }
 		public string BlogContent { get; set; }
+		public string? UserName { get; set; }
+		public DateTime? CreatedDate { get; set; }
+		public int? CreatedBy { get; set; }
+		public DateTime? ModifiedDate { get; set; }
+		public int? ModifiedBy { get; set; }
+		public DateTime? RemovedDate { get; set; }
+		public int? RemovedBy { get; set; }
 		public List<BlogImageDto> BlogImages { get; set; }
 	}
 	public class CreateBlogDto
 	{
 		public string BlogName { get; set; }
 		public string BlogContent { get; set; }
-		//public string CreatedBy { get; set; }
-		// Ảnh tải từ máy
-		public List<IFormFile> Images { get; set; }
+
+		public List<IFormFile>? Images { get; set; }
 
 		// Ảnh từ URL bên ngoài (ví dụ: https://abc.com/image.jpg)
-		public List<string> ImageUrls { get; set; }
+		public List<string>? ImageUrls { get; set; }
 	}
 }
