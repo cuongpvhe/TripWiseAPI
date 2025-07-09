1e79bb59f6c104d95dd2573a4f7304f1b179043c
TungCT <tungcthe176530@fpt.edu.vn>
2025-07-09 10:48:53 +0700

    cập nhật sử dụng google maps, tối ưu prompt

---
+++ Services/IGoogleMapsPlaceService.cs
@@ -0,0 +1,8 @@
+﻿namespace TripWiseAPI.Services
+{
+    public interface IGoogleMapsPlaceService
+    {
+        Task<(double? Latitude, double? Longitude, string? PhotoUrl)> GetPlaceInfoAsync(string placeName);
+    }
+
+}
